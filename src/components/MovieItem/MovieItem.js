09d7b3c5ed3ec8bc2b468e07a02301e76e3d5fa4
Siarhei Samets <siarhei_samets@epam.com>
2022-05-16 16:07:41 +0300

    edit popup

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -8,12 +8,18 @@ class MovieItem extends Component {
     constructor() {
         super();
         this.state = {
-            isOpenDeletePopup: false
+            isOpenDeletePopup: false,
+            isOpenEditPopup: false
         }
     }
     toggleOpenDeletePopup = () => {
         this.setState({
             isOpenDeletePopup: !this.state.isOpenDeletePopup
+        })
+    }
+    toggleOpenEditPopup = () => {
+        this.setState({
+            isOpenEditPopup: !this.state.isOpenEditPopup
         })
     }
     render() {
@@ -38,7 +44,7 @@ class MovieItem extends Component {
                     </div>
                     <div className='item-actions'>
                         <ul>
-                            <li>Edit</li>
+                            <li onClick={this.toggleOpenEditPopup}>Edit</li>
                             <li onClick={this.toggleOpenDeletePopup}>Delete</li>
                         </ul>
                     </div>
@@ -50,6 +56,57 @@ class MovieItem extends Component {
                             <p>Are you sure you want to delete this movie?</p>
                             <div className='toolbar-actions'>
                                 <button className='button-primary'>confirm</button>
+                            </div>
+                        </form>
+                    </>
+                </Popup>
+                <Popup isOpen={this.state.isOpenEditPopup} toggleOpen={this.toggleOpenEditPopup}>
+                    <>
+                        <div className='popup-title'>EDIT MOVIE</div>
+                        <form action="">
+                            <div className='fieldset fieldset-two'>
+                                <div className='field'>
+                                    <label htmlFor="field-title">Title</label>
+                                    <input id='field-title' type="text" placeholder='Enter your title' />
+                                </div>
+                                <div className='field'>
+                                    <label htmlFor="field-date">RELEASE DATE</label>
+                                    <input id='field-date' type="date" placeholder='Select Date' />
+                                </div>
+                            </div>
+                            <div className='fieldset fieldset-two'>
+                                <div className='field'>
+                                    <label htmlFor="field-url">movie url</label>
+                                    <input id='field-url' type="text" placeholder='https://' />
+                                </div>
+                                <div className='field'>
+                                    <label htmlFor="field-rating">RATING</label>
+                                    <input id='field-rating' type="text" placeholder='7.8' />
+                                </div>
+                            </div>
+                            <div className='fieldset fieldset-two'>
+                                <div className='field'>
+                                    <label htmlFor="field-genre">genre</label>
+                                    <select name="genre" id="field-genre">
+                                        <option value="1">Select Genre</option>
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                    </select>
+                                </div>
+                                <div className='field'>
+                                    <label htmlFor="field-runtime">RUNTIME</label>
+                                    <input id='field-runtime' type="text" placeholder='minutes' />
+                                </div>
+                            </div>
+                            <div className='fieldset'>
+                                <div className='field'>
+                                    <label htmlFor="field-overview">OVERVIEW</label>
+                                    <textarea id='field-overview' placeholder='Movie description' />
+                                </div>
+                            </div>
+                            <div className='toolbar-actions'>
+                                <button className='button-secondary'>reset</button>
+                                <button className='button-primary'>submit</button>
                             </div>
                         </form>
                     </>
