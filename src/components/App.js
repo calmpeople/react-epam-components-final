50eac942d5d2ebed30cab11f760ee20a26d7eb62
Siarhei Samets <siarhei_samets@epam.com>
2022-05-17 17:54:40 +0300

    delete movie method

--- src/components/App.js
+++ src/components/App.js
@@ -11,12 +11,57 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            isOpenAddMoviePopup: false
+            isOpenAddMoviePopup: false,
+            movieArray: [
+                {
+                    title: 'Pulp Fiction',
+                    description: 'Action & Adventure',
+                    year: '2004',
+                    id: 1
+                },
+                {
+                    title: 'Bohemian Rhapsody',
+                    description: 'Drama, Biography, Music',
+                    year: '2003',
+                    id: 2
+                },
+                {
+                    title: 'Kill Bill: Vol 2',
+                    description: 'Oscar winning Movie',
+                    year: '1994',
+                    id: 3
+                },
+                {
+                    title: 'Avengers: War of Infinity',
+                    description: 'Action & Adventure',
+                    year: '2004',
+                    id: 4
+                },
+                {
+                    title: 'Inception',
+                    description: 'Action & Adventure',
+                    year: '2003',
+                    id: 5
+                },
+                {
+                    title: 'Reservoir dogs',
+                    description: 'Oscar winning Movie',
+                    year: '1994',
+                    id: 6
+                }
+            ]
         }
     }
     toggleOpenAddMoviePopup = () => {
         this.setState({
             isOpenAddMoviePopup: !this.state.isOpenAddMoviePopup
+        })
+    }
+    removeMovie = (id) => {
+        this.setState(({movieArray}) => {
+            return {
+                movieArray: movieArray.filter(item => item.id !== id)
+            }
         })
     }
     render() {
@@ -25,7 +70,7 @@ class App extends Component {
                 <Header toggleOpen={this.toggleOpenAddMoviePopup} />
                 <MainContent>
                     <ErrorBoundary>
-                        <MovieList />
+                        <MovieList movieArray={this.state.movieArray} removeMovie={this.removeMovie} />
                     </ErrorBoundary>
                 </MainContent>
                 <Footer>
